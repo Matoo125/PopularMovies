44ecdfcf4225cdbc1f560161888c5c11d12a6d24
Matej Vrzala <vrzala.matej.com>
2017-02-04 18:26:42 +0100

    getting data from api

---
+++ app/src/main/java/com/example/matej/popularmoviesdemo/ApiRequest.java
@@ -0,0 +1,18 @@
+package com.example.matej.popularmoviesdemo;
+
+/**
+ * Created by matej on 4.2.2017.
+ * api request data
+ */
+
+public class ApiRequest {
+
+    static public String API_URL = "http://api.themoviedb.org/3/discover/movie";
+    static public String API_KEY = "8510b1c15dd30bd086d3d12db749d9a5";
+    static public String IMAGE_URL = "http://image.tmdb.org/t/p/";
+    static public String IMAGE_SIZE = "w185";
+    static public String IMAGE_NOT_FOUND = "http://www.classicposters.com/images/nopicture.gif";
+
+}
+
+
--- app/src/main/java/com/example/matej/popularmoviesdemo/MainActivity.java
+++ app/src/main/java/com/example/matej/popularmoviesdemo/MainActivity.java
@@ -1,12 +1,156 @@
 package com.example.matej.popularmoviesdemo;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar mProgressBar;
+
+    private String TAG = MainActivity.class.getSimpleName();
+
+    ArrayList<HashMap<String, String>> moviesList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+        mProgressBar = (ProgressBar)findViewById(R.id.progressBar);
+
+        moviesList = new ArrayList<>();
+
+        new GetMovies().execute();
+
+    }
+
+
+    /**
+     * Async task class to get json by making HTTP call
+     */
+    private class GetMovies extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            mProgressBar.setVisibility(View.VISIBLE);
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+
+            // build url
+            Uri uri = Uri.parse(ApiRequest.API_URL).buildUpon()
+                    .appendQueryParameter("sort_by", "popular")
+                    .appendQueryParameter("api_key", ApiRequest.API_KEY)
+                    .build();
+
+            URL url;
+            String jsonStr = null;
+            try {
+                url = new URL(uri.toString());
+                // Making a request to url and getting response
+                jsonStr = sh.makeServiceCall(url);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+
+
+            Log.i(TAG, "Response from url: " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObject = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    JSONArray movies = jsonObject.getJSONArray("results");
+
+                    // looping through all movies
+                    for (int i = 0; i < movies.length(); i++) {
+                        JSONObject m = movies.getJSONObject(i);
+
+                        String id = m.getString("id");
+                        String title = m.getString("title");
+                        String poster = m.getString("poster_path");
+
+                        // tmp hash map for single movie
+                        HashMap<String, String> movie = new HashMap<>();
+
+                        // adding each child node to HashMap array key => value
+                        movie.put("id", id);
+                        movie.put("title", title);
+                        movie.put("poster", poster);
+
+                        // adding movie to movies list
+                        moviesList.add(movie);
+                    }
+
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            // Dismiss the progress dialog
+            mProgressBar.setVisibility(View.INVISIBLE);
+
+            /**
+             * Updating parsed JSON data into Grid View
+             */
+            Log.i("Movies", moviesList.toString());
+
+
+        }
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -34,14 +178,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(myToolbar);
-
-
-    }
 }
